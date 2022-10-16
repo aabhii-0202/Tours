ba1c06ef91ab3a028a348a6f824d5886e14838dc
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-10-16 22:26:10 +0530

    In between signup

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,10 +1,22 @@
 import API from './api';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const login = async (credentials) => {
     return await API({
         method: 'POST',
         url: '/users/login',
+        data: credentials,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((result) => { return result.data; })
+    .catch(err => { return err; });
+};
+
+export const signup = async (credentials) => {
+    return await API({
+        method: 'POST',
+        url: '/users/signup',
         data: credentials,
         headers: {
             'Content-Type': 'application/json',
--- app/screens/Signup.js
+++ app/screens/Signup.js
@@ -1,28 +1,83 @@
 import React,{useLayoutEffect, useState} from 'react';
 import {
     SafeAreaView,ScrollView,
-    StyleSheet,Text, View,
-    TouchableOpacity,
+    StyleSheet,Text,Dimensions,
+    TouchableOpacity,Keyboard,
 } from 'react-native';
 import { Colors, FontSizes } from '../helper/theme';
 import { BtnSolid } from '../components/Buttons';
 import DropDown from '../components/DropDown';
 import Input from '../components/Input';
+import Spinner from 'react-native-loading-spinner-overlay';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Snackbar } from 'react-native-paper';
+import { signup } from '../api/auth';
 
 const App = ({navigation}) => {
 
-    const [name,setName] = useState('');
-    const [mail,setMail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirm, setConfirm] = useState('');
+    const [name,setName] = useState('test');
+    const [mail,setMail] = useState('user5@gmail.com');
+    const [password, setPassword] = useState('test1234');
+    const [confirm, setConfirm] = useState('test1234');
+    const [loading, setloading] = useState(false);
+    const [snackbar, setsnackbar] = useState(false);
+    const [snackbarText, setsnackbarText] = useState('');
 
     const data = ['user','guide','lead-guide'];
     const [selected,setSelected] = useState();
 
 
+    const signupClicked = async () => {
+        Keyboard.dismiss();
+        if (mail === '' || password === '' || confirm === '' || name === '') {
+            setsnackbarText('Please Fill all the credentials');
+            setsnackbar(true);
+        }
+        else {
+            let credentials = {
+                'name': name,
+                'email': mail,
+                'password': password,
+                'passwordConfirm': confirm,
+                'role': selected,
+            };
+            setloading(true);
+            const res = await signup(credentials);
+
+            if (res.status === 'success'){
+                await AsyncStorage.setItem('@token',res.token);
+                await AsyncStorage.setItem('@id',res.data.user.photo);
+                await AsyncStorage.setItem('@name',res.data.user.name);
+                await AsyncStorage.setItem('@email',res.data.user.email);
+                setloading(false);
+                navigation.navigate('NavMain', {screen: 'Home'});
+            }
+            else {
+                console.log(res);
+                setsnackbarText('Error Logging In Try Again Later');
+                setsnackbar(true);
+                setloading(false);
+            }
+        }
+        navigation.navigate('NavMain', {screen: 'Home'});
+    };
+
+
     return (
         <SafeAreaView style={{flex:1,backgroundColor:Colors.background,paddingHorizontal:20}}>
             <ScrollView showsVerticalScrollIndicator={false}>
+            <Spinner
+                visible={loading}
+                textContent={'Please Wait...'}
+                textStyle={{ color: '#FFF' }}
+            />
+            <Snackbar
+                visible={snackbar}
+                onDismiss={()=>setsnackbar(false)}
+                style={{ width: Dimensions.get('window').width - 15 }}
+                action={{
+                label: 'Ok',
+                }}>{snackbarText}</Snackbar>
             <Text style={styles.title}>Create New Account</Text>
             <Input
                 text="Name"
@@ -54,7 +109,7 @@ const App = ({navigation}) => {
                 setSelected={setSelected}
             />
 
-            <BtnSolid text="Submit"  click={()=>navigation.navigate('NavMain', {screen: 'Home'})} />
+            <BtnSolid text="Submit"  click={signupClicked} />
             <TouchableOpacity onPress={()=>navigation.navigate('NavAuth', {screen: 'Login'})}>
             <Text style={styles.t1}>Already Have an Account</Text>
             <Text style={styles.t2}>Login Insted</Text>
