e7f5ac26d061b1ae1ab90be876ad66db936e491e
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-11-23 23:56:51 +0530

    URL changed

--- app/api/api.js
+++ app/api/api.js
@@ -3,5 +3,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://abhishektours.herokuapp.com/api/v1"
+  // baseURL: "https://abhishektours.herokuapp.com/api/v1"
+  // baseURL: "http://192.168.1.15:3000/api/v1"
+  baseURL: "http://192.168.1.6:3000/api/v1"
 });
