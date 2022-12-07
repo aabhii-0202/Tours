993544bbf91e1b478a182a59a0ccb98601fe9368
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-12-07 21:47:38 +0530

    Merger

--- app/api/api.js
+++ app/api/api.js
@@ -4,5 +4,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: "https://abhishektours.herokuapp.com/api/v1"
-  baseURL: "http://3.110.208.160/api/v1"
+  baseURL: "http://3.110.208.160:3000/api/v1"
 });
--- app/api/tours.js
+++ app/api/tours.js
@@ -1,11 +1,18 @@
 import API from './api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
-export const getAllTour = async () => {
+export const getAllTours = async () => {
     return await API({
         method: 'GET',
         url: '/tours/',
     })
+    .then(result => {return result.data;})
+    .catch(err => {return err;});
+};
+
+export const getAllTour = async () => {
+    return await axios.get('http://3.110.208.160:3000/api/v1/tours/')
     .then(result => {return result.data;})
     .catch(err => {return err;});
 };
